bba6d8a66be51d392aabc28e4923ca14740c0d1b
uni_note_007 <ushiriscco@gmail.com>
2019-12-04 19:08:56 +0900

    add compute
    色が付きました

---
+++ pws4_12_1/Assets/Script/Fractal.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fractal : MonoBehaviour
+{
+    float timer = 0.5f;
+    public GameObject cube;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        if(timer<=0)
+        {
+            var child = Instantiate(gameObject);
+            var child2 = Instantiate(gameObject);
+            child.gameObject.transform.position += new Vector3(Random.Range(-0.5f, 0.5f), Random.Range(-0.5f, 0.5f), Random.Range(-0.5f, 0.5f));
+            child2.gameObject.transform.position += new Vector3(Random.Range(-0.5f, 0.5f), Random.Range(-0.5f, 0.5f), Random.Range(-0.5f, 0.5f));
+            Destroy(this);
+        }
+    }
+}
